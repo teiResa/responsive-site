8d7942356a08c9cf827cc2e206b1f560bedcdd72
Theresa <49374955+teiResa@users.noreply.github.com>
2023-12-01 01:02:53 +0800

    landing page
    Wanted to challenge myself to a "before bed" project. In total, I spent about a 1hr20. Not bad.

--- strap/fruitstand/pages/js/greeting.js
+++ strap/fruitstand/pages/js/greeting.js
@@ -1,19 +1,25 @@
-
-var currDate = new Date();
-var currHour = currDate.getHours();
+var today = new Date();
+var hourNow = today.getHours();
 var greeting;
 var icon;
 
-if (currHour > 18) {
- greeting = 'Swell Evening';
- icon = moon-o
-} else if (currHour > 12) {
- greeting = 'Good afternoon';
-} else if (currHour > 0) {
- greeting = 'Moooornin.';
-} else {
- greeting = 'Heya, welcome!';
+if (hourNow < 12){
+  greeting = "Moooornin";
+  icon = "coffee";
 }
-document.getElementById('greeting').innerHTML = ('<h3>' + greeting + '<h3>');
+else if (hourNow < 20){
+  greeting = 'Good afternoon!';
+  icon = "sun-o";
+}
+else if (hourNow < 24){
+  greeting = "Good evening"
+  icon = "moon-o";
+}
+else {
+  greeting = "Heeeya, welcome";
+}
+document.getElementById('greeting1').innerHTML = ('<h1>' + greeting + '<h1>');
+
+//document.getElementById('user-instructions').classList.add('pinkTest');
 
 document.getElementById("icon").innerHTML = ('<i class="fa fa-' + icon + '" aria-hidden="true"></i>');
